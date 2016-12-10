2e38e02762dae999125a0811a49941310db942a5
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-12-10 13:01:46 -0600

    start up

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+
+<?php require_once('header.php'); ?>
+
+<body>
+
+
+PBS
+
+
+<br><br><br><br><br>
+<?php require_once('footer.php'); ?>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+      	
+	<div class="container">
+    	<div class="navbar-header">
+          
+		<button type="button" class="navbar-toggle btn-custom" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+         	<span class="sr-only">Toggle navigation</span>
+         	<span class="icon-bar"></span>
+        </button>
+         
+         <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="50"></a>		
+     
+     	</div>
+
+         
+	 <div id="navbar" class="collapse navbar-collapse">
+          	
+		<ul class="nav navbar-nav">
+		
+		<li><a  href="index.php">Home</a></li>
+    <li><a  href="about.php">About</a></li>
+    <li><a  href="contact.php">Contact</a></li>
+
+
+            					
+       </div>
+   </div>
+
+
+
+
+
+ </nav>
