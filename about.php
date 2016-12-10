0e9b5004b6555374ec9610a021ceaa32a470ff52
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-12-10 14:46:10 -0600

    style

--- about.php
+++ about.php
@@ -16,7 +16,7 @@
 </div>
 <div class="row">
 <div class="col-xs-4">
-<img src="assets/img/logo.png" width="100%">
+<img src="assets/img/logo.png">
 </div>
 <div class="col-xs-8">
 <br><p> Men's professional haircuts. Click <a href="services.php">here</a> for pricing. Appointments only. Click<a href="contact.php"> here </a>to schedule an appointmnet. Locally owned since 2002. Previously located on Puetz road in Franklin, Wi. </p>
