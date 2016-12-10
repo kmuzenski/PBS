06eea0589b0249fd44b74751f89c53490fea8986
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-12-10 14:09:18 -0600

    +text

--- index.php
+++ index.php
@@ -7,16 +7,33 @@
 <body>
 <?php require_once('nav.php'); ?>
 <br><br><br><br><br>
+
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-md-12 col-lg-12">
 <center>
-	
-<h1> Pauls Barber Shop </h1> 
-<br>
+<h1> Pauls Barber Shop </h1> <br>
+</center>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-3 col-md-3 col-lg-3">
+</div>
+<div class="col-xs-6 col-md-6 col-lg-6">
 <img src="assets/img/main.jpg">
 <br>
+</div>
+<div class="col-xs-3 col-md-3 col-lg-3">
+</div>
+</div>
+<div class="row">
+<center>
 <p> 10529 W Forest Home Ave Hales Corners, WI 53132 </p>
 <br>
-<p> Locally owned. Call 414-777-7777 to book an appointment </p>
+<p> Locally owned. Call 414-388-6807 to book an appointment </p>
 </center>
+</div>
+</div>
 
 <br><br><br><br><br>
 <?php require_once('footer.php'); ?>
