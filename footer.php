4092ca087b7650bcf327c335bc12d52ad91b2fbf
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-12-19 13:22:55 -0600

    style

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<footer>Paul's Barbershop &copy; <a href="sitemap.php" alt="sitemap" title="sitemap">Site Map</a></footer>
+<footer><center><p>Paul's Barbershop &copy; <a href="sitemap.php" alt="sitemap" title="sitemap">Site Map</a></p></center></footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
--- sitemap.php
+++ sitemap.php
@@ -7,15 +7,33 @@
 <body>
 <?php require_once('nav.php'); ?>
 <br><br><br><br><br>
-
+<div class="container">
+<div class="row">
+<div class="col-xs-12">
+<center>
 <h1>Site Map</h1>
 <br><br><br>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12">
+
 <h3><u><a href="index.php" alt="home" title="home">Home Page</a></u></h3>
 <br><br>
+</div>
+</div>
+
+<div class="row">
+<div class="col-xs-12">
 <h3><u><a href="about.php" alt="About Paul's Buisness" title="About Paul's Buisness">About</a></u></h3>
 <br><br>
-<h3><u><a href="contact.php" alt="Contact page">Contact</a></u></h3>
+</div></div>
 
+<div class="row">
+<div class="col-xs-12">
+<h3><u><a href="contact.php" alt="Contact page">Contact</a></u></h3>
+</center>
+</div></div>
 
 <br><br><br><br><br>
 <?php require_once('footer.php'); ?>
