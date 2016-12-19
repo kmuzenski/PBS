a6f733c246eb31f1a43b9415aade77d243af257b
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-12-19 13:25:18 -0600

    style

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<footer><center><p>Paul's Barbershop &copy; <a href="sitemap.php" alt="sitemap" title="sitemap">Site Map</a></p></center></footer>
+<footer><center><p id="footer">Paul's Barbershop &copy; <a href="sitemap.php" alt="sitemap" title="sitemap">Site Map</a></p></center></footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
