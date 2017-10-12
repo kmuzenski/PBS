e031af9ccac5854e1902fa69e5285fada2c00ebb
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-12 18:46:30 -0500

    nav test

--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
   
   <div class="col-xs-12 col-md-4 col-lg-4" id="navborder" >
   <center>
-  <p><a href="about.php" id="mynav" title="gallery">About</a></p>
+  <a href="about.php" id="mynav" title="gallery">About</a>
   </center>
   </div>
 
