93dab23edaa0d9747777df28ee5d17daf93c5f77
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-12 19:30:54 -0500

    + google map feature

--- about.php
+++ about.php
@@ -32,6 +32,15 @@
 </div>
 </div>
 
+<br><br>
+
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-md-12 col-lg-12">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2920.921919798564!2d-88.04631004931895!3d42.937772906312276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88050e645033b359%3A0x97d17a5e62d80566!2s10529+W+Forest+Home+Ave%2C+Hales+Corners%2C+WI+53130!5e0!3m2!1sen!2sus!4v1507854572188" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+</div>
+</div>
 <br><br><br><br><br>
 <?php require_once('footer.php'); ?>
 
