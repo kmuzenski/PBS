a8d407964a5e39fc662c2d4b125c2dad079a1dd8
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-12 19:20:17 -0500

    style test

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
 <div class="container">
 <div class="row">
 <div class="col-xs-2 col-md-2 col-lg-2">
-<img src="assets/img/logo.png" width="100%">
+<img src="assets/img/logo.png" width="75%">
 </div>
 <div class="col-xs-10 col-md-10 col-lg-10">
 <h1>Paul's Buisness</h1>
@@ -20,8 +20,15 @@
 </div>
 
 <br>
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-md-12 col-lg-12">
+<center>
 <p> Men's professional haircuts. Appointments only. Click<a href="contact.php"> here </a>to schedule an appointmnet. Locally owned since 2002. Previously located on Puetz road in Franklin, Wi. </p>
-
+</center>
+</div>
+</div>
+</div>
 
 <br><br><br><br><br>
 <?php require_once('footer.php'); ?>
